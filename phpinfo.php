54f7af03c2951a2db81e279cb3752faad28bda8d
Jan-Willem Westerhof <info@westit.nl>
2025-10-07 12:23:50 +0200

    Added PHPInfo

---
+++ phpinfo.php
@@ -0,0 +1,5 @@
+<?php
+echo "Server Software: " . $_SERVER['SERVER_SOFTWARE'] . "\n";
+echo "Server Name: " . $_SERVER['SERVER_NAME'] . "\n";
+echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "\n";
+phpinfo();
